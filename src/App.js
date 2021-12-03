9027c88ca84e074184505b8173f2f3a85fce5537
Adrian Walker <awalker3365@gmail.com>
2021-12-03 12:53:28 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import { Link, Switch, Route } from 'react-router-dom';
+import { Link, Routes, Route, } from 'react-router-dom';
+import React from 'react';
 import './style.css';
+
+import Home from "./Home"
+import About from "./About"
+import Services from "./Services"
 
 function App() {
   return (
     <div>
-      <Link to="/"></Link>
-      <Link to="./about"></Link>
-      <Link to="./services"></Link>
+      <nav className="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/services">Services</Link>
+      </nav>
+
+
+
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/services" element={<Services />} />
+      </Routes>
+
+      <footer className="footer">
+        <h2>Footer</h2>
+      </footer>
+
     </div>
   );
 }
---
+++ src/Services.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Services() {
+    return (
+        <div className="services">
+            <h2>Plumbing</h2>
+            <h2>Drains</h2>
+            <h2>Sewer</h2>
+            <h2>Gas Plumbing</h2>
+        </div>
+    )
+}
+
+export default Services
